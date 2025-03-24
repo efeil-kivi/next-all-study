39db612271d1b04ad46a148f0f04814ea67123e4
efeil-kivi <zmc863754060@gmail.com>
2025-03-25 00:30:19 +0800

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
         </div>
         <Button>
           <a
-            className={styles.button}
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://github.com/nextui-org/nextui"
             target="_blank"
             rel="noopener noreferrer"
